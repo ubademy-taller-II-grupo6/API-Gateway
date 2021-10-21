d3fe93d3a38b75701b3014113d8d141826845700
Marcos Riarte <mriarte@fi.uba.ar>
2021-10-21 16:24:33 -0300

    Lista de microservicios ruteados y sus endpoints

--- router/DinamycRouter.py
+++ router/DinamycRouter.py
@@ -2,21 +2,20 @@ import requests
 
 class DinamycRouter:
 
-def _init_self(self,url):
+
+ def _init_self(self,url):
     self.url = url
 
-def getUrl(self):
+ def getUrl(self):
         return self.url
 
-def GETJson(self):
+ def GETJson(self):
    resp = requests.get(self.url)
-   if resp.status_code != 200:
-     print "Erro en el server" + self.url
+   if resp.status_code != 200: print ("Erro en el server" + self.url)
    respuesta = resp.json()
    return respuesta
 
-#{'firstname':'Ryan', 'lastname':'Mitchell'}#
-
-def POSTJson(self,json):
-    resp = requests.post(self.url, data = json)
+ #{'firstname':'Ryan', 'lastname':'Mitchell'}#
+ def POSTJson(self, json):
+    resp = requests.post(self.url, data=json)
     return resp.status_code
