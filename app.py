c9d6292ab53ab79deee4b5a227c6eecc7e94df8e
Marcos Riarte <mriarte@fi.uba.ar>
2021-11-14 05:31:32 -0300

    Se corrige los permisos de la app

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from routers.APIGateway import APIGateway
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title= "Ubademy-APIGateway de usuarios",
     description= "API gateway ubademy, implementa la autenticacion e interfaz de servicios",
     version=1.0
 )
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"],
+                   allow_headers=["*"])
 
 app.include_router(APIGateway)
 
